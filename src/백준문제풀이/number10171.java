ef2e190ba026759d109e8ef81fa29b53c2b35adc
colorfulapple <zn30_@naver.com>
2023-01-10 22:12:36 +0900

    10171번

---
+++ src/백준문제풀이/number10171.java
@@ -0,0 +1,12 @@
+package 백준문제풀이;
+
+public class number10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
