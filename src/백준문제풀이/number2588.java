9860232010216f352e1fa08ea9eb10133d34f016
colorfulapple <zn30_@naver.com>
2023-01-10 22:02:24 +0900

    2588번

---
+++ src/백준문제풀이/number2588.java
@@ -0,0 +1,26 @@
+package 백준문제풀이;
+
+import java.util.Scanner;
+
+public class number2588 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int fir = sc.nextInt();
+		int sec = sc.nextInt();
+		
+		int hun = sec/100;
+		int ten = sec/10 - hun*10;
+		int one = sec - hun*100 - ten*10;
+
+		int three = fir*one;
+		int four = fir*ten;
+		int five = fir*hun;
+		
+		System.out.println(three);
+		System.out.println(four);
+		System.out.println(five);
+		System.out.println(three + four*10 + five*100);
+	}
+
+}
